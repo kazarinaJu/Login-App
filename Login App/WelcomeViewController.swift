65a2b3623df7942167686e58706a683251ba8a5f
Julia Kazarina <yastrebovayv@gmail.com>
2023-04-01 18:35:57 +0500

    create welcome view controller

---
+++ Login App/WelcomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeViewController.swift
+//  Login App
+//
+//  Created by Юлия Ястребова on 01.04.2023.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
