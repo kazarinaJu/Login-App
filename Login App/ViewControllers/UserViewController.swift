90b4c9124509401eaa033417e559bb3fdd9e03b5
Julia Kazarina <yastrebovayv@gmail.com>
2023-04-05 18:58:59 +0500

    avatar added

--- Login App/ViewControllers/UserViewController.swift
+++ Login App/ViewControllers/UserViewController.swift
@@ -9,16 +9,14 @@ import UIKit
 
 final class UserViewController: UIViewController {
     
-    
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var surnameLabel: UILabel!
     @IBOutlet var cityLabel: UILabel!
     @IBOutlet var companyLabel: UILabel!
     @IBOutlet var jobTitleLabel: UILabel!
+    @IBOutlet var userImage: UIImageView!
     
     var user: User!
-    
-   
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +26,9 @@ final class UserViewController: UIViewController {
         cityLabel.attributedText = makeItalic(string: user.person.city)
         companyLabel.attributedText = makeItalic(string: user.person.companyName)
         jobTitleLabel.attributedText = makeItalic(string: user.person.jobTitle)
+        
+        userImage.layer.cornerRadius = userImage.frame.size.width / 2
+        userImage.clipsToBounds = true
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
