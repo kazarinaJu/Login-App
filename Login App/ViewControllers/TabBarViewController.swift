e85ce453e7847f92622cb0f79ba1eef09709213a
Julia Kazarina <yastrebovayv@gmail.com>
2023-04-05 14:27:23 +0500

    created TabBarViewController

---
+++ Login App/ViewControllers/TabBarViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TabBarViewController.swift
+//  Login App
+//
+//  Created by Юлия Ястребова on 05.04.2023.
+//
+
+import UIKit
+
+final class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
