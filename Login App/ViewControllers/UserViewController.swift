573ae6163af10d33073f00ef3f872c18210d958e
Julia Kazarina <yastrebovayv@gmail.com>
2023-04-05 14:27:00 +0500

    created UserVC

---
+++ Login App/ViewControllers/UserViewController.swift
@@ -0,0 +1,43 @@
+//
+//  UserViewController.swift
+//  Login App
+//
+//  Created by Юлия Ястребова on 05.04.2023.
+//
+
+import UIKit
+
+final class UserViewController: UIViewController {
+    
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var surnameLabel: UILabel!
+    @IBOutlet var cityLabel: UILabel!
+    @IBOutlet var companyLabel: UILabel!
+    @IBOutlet var jobTitleLabel: UILabel!
+    
+    var user: User!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameLabel.attributedText = makeItalic(string: user.person.name)
+        surnameLabel.attributedText = makeItalic(string: user.person.surname)
+        cityLabel.attributedText = makeItalic(string: user.person.city)
+        companyLabel.attributedText = makeItalic(string: user.person.companyName)
+        jobTitleLabel.attributedText = makeItalic(string: user.person.jobTitle)
+    }
+}
+
+extension UserViewController {
+    func makeItalic(string: String) -> NSAttributedString {
+        let italicAttribute = [NSAttributedString.Key.font: UIFont.italicSystemFont(ofSize: 15)]
+        
+        let attributedString = NSMutableAttributedString(string: string)
+        attributedString.addAttributes(italicAttribute, range: NSRange(location: 0, length: string.count))
+        
+        return attributedString
+    }
+}
+
+
